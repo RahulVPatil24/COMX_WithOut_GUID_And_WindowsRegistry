a5c088acb7580f874cad80c12ff92da0c8a95e07
RahulVPatil24 <rp98514360@gmail.com>
2022-11-16 23:50:12 +0530

    Add files via upload

---
+++ COMX/Server/ClassFactoryDllServerWithRegFile.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "ComInterfaceX.h"
+
+class ISum:public IMyUnknown
+{
+public: 
+	virtual HRESULT __stdcall SumOfTwoIntigers(int, int, int *)=0; 
+};
+
+class ISubtract:public IMyUnknown
+{
+public:
+	virtual HRESULT __stdcall SubOfTwoIntigers(int, int, int *)=0; 
+};
+
+
+const long CLSID_CSumSubtract = 3131;
+
+const long IID_ISum = 3132;
+
+const long IID_ISubtract = 3133;
